07c9942f9da28762e8f9df2700047ec60bf95989
PrasanthiKolli <kolliprasanthi14@gmail.com>
2022-12-09 18:12:49 +0530

    front end angular initial commit

---
+++ FrontEnd/angular-11-client/src/app/_services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const USER_API = 'http://localhost:8091/api/v1/digitalbooks/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(userName: string, password: string): Observable<any> {
+    return this.http.post(USER_API + 'sign-in', {
+      userName,
+      password
+    }, httpOptions);
+  }
+
+  register(userName: string, emailId: string, password: string,phoneNumber:string,role:any): Observable<any> {
+    return this.http.post(USER_API + 'sign-up', {
+      userName,
+      emailId,
+      password,
+      phoneNumber,
+      role
+    }, httpOptions);
+  }
+}
